a53920187bf329f5479ce4d00f94064a9c8d3929
AsherFLacson <asher.lacson@smc.pshs.edu.ph>
2026-03-02 17:46:54 +0800

    Add files via upload

---
+++ LacsonQ3_FA03.py
@@ -0,0 +1,13 @@
+
+import numpy as pyn
+
+chess_players = ["Magnus", "Hikaru", "Gukesh"]
+ELO = pyn.array([
+    [12, -8, 14, 9, 5],
+    [-5, -9, 10, -7, 14],
+    [-6, 13, -12, -9, 12]
+    ])
+
+for row in ELO:
+    print(row[1])
+
